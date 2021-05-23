34ccc2ea715449b783afe1e49d249b3917dbfe07
Anwesh43 <anweshthecool0@gmail.com>
2021-05-23 17:47:18 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ const strokeFactor : number = 90
 const sizeFactor : number = 6.9 
 const delay : number = 20 
 const backColor : string = "#bdbdbd"
+const parts : number = 3
+const scGap : number = 0.02 / parts 
 const colors : Array<string> = [
     "#f44336",
     "#9C27B0",
@@ -24,5 +26,42 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBiRectUpBar(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, -size / 2, size / 2 - size * sf1, -size / 2, size / 2)
+            context.fillRect(-size / 2, -size / 2, size * 0.25 * sf2, size)
+            context.restore()
+        }
+        context.fillRect(-0.25 * size, size / 2 - size * sf3, 0.5 * size, size * sf3)
+        context.restore()
+    }
+
+    static drawBRUBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBiRectUpBar(context, scale)
     }
 }
